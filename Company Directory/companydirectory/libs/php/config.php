ca2f46f1d7f2c2271a5be29e4a751e40de108965
bluezoopla <59399623+Undraoid@users.noreply.github.com>
2021-07-05 15:51:44 +0100

    Update config.php

--- Company Directory/companydirectory/libs/php/config.php
+++ Company Directory/companydirectory/libs/php/config.php
@@ -7,9 +7,9 @@
 	#$cd_password = "greenxampp"; // password
 	#$cd_dbname = "dbs2701365"; // database name
 
-        $host_name = 'db5003338517.hosting-data.io';
-	$database = 'dbs2701365';
-	$user_name = 'dbu2510725';
-	$password = 'greenmysql12';
+	$host_name = 'db5003338517.hosting-data.io';
+  	$database = 'dbs2701365';
+  	$user_name = 'dbu2510725';
+	$password = 'Pass1!111';
 
 ?>

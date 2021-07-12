7ee409f067e219bb309c741f2f00732176a0abbb
bluezoopla <59399623+Undraoid@users.noreply.github.com>
2021-07-12 16:14:37 +0100

    staff add

--- Company Directory/js/dataDisplay.js
+++ Company Directory/js/dataDisplay.js
@@ -1,6 +1,6 @@
 //companydirectory/libs/php/getDepartmentByID.php
 
-$(document).ready(function() {
+//$(document).ready(function() {
     $('#searchButton').click(function(){
         $.ajax({
             type: 'GET',
@@ -25,7 +25,7 @@ for(var i=0; i<result.data.length; i+=5){
       <td>${result.data[i].email}</td>
       <td>${result.data[i].name}</td>
     </tr>`
-
+  
      table.append(row);
   
 }
@@ -33,4 +33,48 @@ for(var i=0; i<result.data.length; i+=5){
         });
     
     })
+  
+  $('#InputSelect3').change(function(){
+    $.ajax({
+        type: 'GET',
+            url: 'companydirectory/libs/php/filterLocationId.php',
+            dataType: 'json',
+            data: {
+             departmentId: $('#InputSelect3').val()
+            },
+            success: function(result){
+              console.log(result.data[i]);
+                var closeButton = $("#InputSelect2");
+                closeButton.empty();
+
+                for(var i=0; i<result.data.length; i+=5){
+
+                   var select = `
+                    <option>${result.data[i].firstName + " " + result.data[i].lastName}</option>
+                    `
+                
+                     closeButton.append(select);
+                  
+                }
+            }
+    })
 })
+  
+$('#buttonAdd').click(function(){
+    $.ajax({
+        type: 'GET',
+        url: 'companydirectory/libs/php/addStaff.php',
+        data: {
+         name: $("#fn"),
+         surname: $("#ln"),
+         email: $("#email"),
+         departmentId: $("#InputSelect3").val()
+        },
+        success: function(result){
+console.log("New Staff Added!" + result.data);
+        }
+    })
+})
+
+  
+//})

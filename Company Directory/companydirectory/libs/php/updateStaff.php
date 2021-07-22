39a15a8b691355590a4f875f93ac5b76b6ef9080
bluezoopla <59399623+Undraoid@users.noreply.github.com>
2021-07-22 12:21:27 +0100

    Update updateStaff.php

--- Company Directory/companydirectory/libs/php/updateStaff.php
+++ Company Directory/companydirectory/libs/php/updateStaff.php
@@ -13,13 +13,6 @@ $executionStartTime = microtime(true);
 
 include("config.php");
 header('Content-Type: application/json; charset=UTF-8');
-$data = [];
-
-$output['status']['code'] = "200";
-$output['status']['name'] = "ok";
-$output['status']['description'] = "success";
-$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-$output['data'] = $data;
 
 $conn = new mysqli($host_name, $user_name, $password, $database);
 
@@ -41,28 +34,35 @@ if (mysqli_connect_errno()) {
 
 // $_REQUEST used for development / debugging. Remember to cange to $_POST for production
 
+if (!isset($_REQUEST['info']) || empty($_REQUEST['info']) || $_REQUEST['info'] == '') {
+    $output['status']['code'] = "400";
+    $output['status']['name'] = "executed";
+    $output['status']['description'] = "Info required.";
+    $output['data'] = [];
+    echo json_encode($output);
+    die();
+}
+
 if (!isset($_REQUEST['staff']) || empty($_REQUEST['staff']) || $_REQUEST['staff'] == '') {
     $output['status']['code'] = "400";
     $output['status']['name'] = "executed";
     $output['status']['description'] = "Staff required.";
     $output['data'] = [];
+    echo json_encode($output);
+    die();
 }
 
-if (!isset($_REQUEST['info']) || empty($_REQUEST['info']) || in_array($_REQUEST['info'], array('firstName', 'lastName', 'email', 'departmentID')) == false) {
+if (!isset($_REQUEST['previous']) || empty($_REQUEST['previous']) || $_REQUEST['previous'] == '') {
     $output['status']['code'] = "400";
     $output['status']['name'] = "executed";
-    $output['status']['description'] = "no such info.";
+    $output['status']['description'] = "Previous required.";
     $output['data'] = [];
+    echo json_encode($output);
+    die();
 }
 
-if (!isset($_REQUEST['value'])) {
-    $output['status']['code'] = "400";
-    $output['status']['name'] = "executed";
-    $output['status']['description'] = "value required.";
-    $output['data'] = [];
-}
-
-$query = "UPDATE personnel SET ".$_REQUEST['info']."='".$_REQUEST['value']."'"." WHERE firstName='" . $_REQUEST['staff']."'";
+$query = 'UPDATE personnel SET personnel.'.$_REQUEST['info'].' = "'.$_REQUEST['staff'].'" WHERE personnel.firstName = "'.$_REQUEST['previous'].'"';
+echo $query;
 $result = $conn->query($query);
 if (!$result) {
 
@@ -70,8 +70,21 @@ if (!$result) {
     $output['status']['name'] = "executed";
     $output['status']['description'] = "query failed";
     $output['data'] = [];
+
+    mysqli_close($conn);
+    echo json_encode($output);
+
+    die();
+
 }
 
+$data = [];
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+$output['data'] = $data;
 
 mysqli_close($conn);
 

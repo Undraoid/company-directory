d5865043367723ad7df8d4143efb2c01df8c0343
bluezoopla <59399623+Undraoid@users.noreply.github.com>
2021-07-27 10:51:08 +0100

    Update locationDisplay.js

--- Company Directory/js/locationDisplay.js
+++ Company Directory/js/locationDisplay.js
@@ -1,104 +1,108 @@
 $(document).ready(function () {
 
-  $.ajax({
-      type: 'GET',
-      url: 'companydirectory/libs/php/getLocation.php',
-      dataType: 'json',
-      success: function (result) {
-          var table = $("tbody");
-          table.empty();
-
-          for (var i = 0; i < result.data.length; i ++) {
-
-              var row = `
-<tr>
-<td id="tableLocation">${result.data[i].name}<a href="#" id="removeUser" onclick="resetVal()"><i class="fas fa-user-alt-slash" style="float:right; margin-left:50px; color:red;" id="iconSettings"></i></a><a href="#" id="editUser" onclick="valReset()" data-toggle="modal" data-target="#updateModal"><i class="fas fa-user-edit" style="float:right;" id="iconSettings"></i></a></td>
-</tr>`
-
-              table.append(row);
-
-          }
-      }
-  });
-
-  function reset(){
-      $.ajax({
-          type: 'GET',
-          url: 'companydirectory/libs/php/getLocation.php',
-          dataType: 'json',
-          success: function (result) {
-              var table = $("tbody");
-              table.empty();
+    $.ajax({
+        type: 'GET',
+        url: 'companydirectory/libs/php/getLocation.php',
+        dataType: 'json',
+        success: function (result) {
+            var table = $("tbody");
+            table.empty();
   
-              for (var i = 0; i < result.data.length; i ++) {
+            for (var i = 0; i < result.data.length; i ++) {
   
-                  var row = `
+                var row = `
   <tr>
-    <td id="tableLocation">${result.data[i].name}<a href="#" id="removeUser" onclick="resetVal()"><i class="fas fa-user-alt-slash" style="float:right; margin-left:50px; color:red;" id="iconSettings"></i></a><a href="#" id="editUser" onclick="valReset()" data-toggle="modal" data-target="#updateModal"><i class="fas fa-user-edit" style="float:right;" id="iconSettings"></i></a></td>
+  <td id="tableLocation">${result.data[i].name}<a href="#" id="removeUser" onclick="resetVal()"><i class="fas fa-user-alt-slash" style="float:right; margin-left:50px; color:red;" id="iconSettings"></i></a><a href="#" id="editUser" onclick="valReset()" data-toggle="modal" data-target="#updateModal"><i class="fas fa-user-edit" style="float:right;" id="iconSettings"></i></a></td>
   </tr>`
   
-                  table.append(row);
+                table.append(row);
   
-              }
-          }
-      });
-  }
-
-
-
-$(document).on('click', '#buttonAdd', function(e) {
-      $.ajax({
-          type: 'POST',
-          url: 'companydirectory/libs/php/addLocation.php',
-          data: {
-              name: $("#fn").val(),
-          },
-          success: function (result) {
-              console.log("New Staff Added!" + result.data);
-             $('#myModal').modal('toggle');
-             reset();
-          }
-      })
-      $(this).unbind('submit').submit()
-  })
-
-
-
-
-$(document).on('click', '#removeUser', function(e) {
+            }
+        }
+    });
+  
+    function reset(){
+      console.log("refreshing")
+        $.ajax({
+            type: 'GET',
+            url: 'companydirectory/libs/php/getLocation.php',
+            dataType: 'json',
+            success: function (result) {
+                var table = $("tbody");
+                table.empty();
     
-      var validate = confirm("Are you sure you want to remove this user?");
-      var removeIcons = $("i#iconSettings.fas.fa-user-alt-slash");
-
-if(validate == true){
-for(var i=0;i<removeIcons.length;i++){
-removeIcons[i].onclick = function(){
-   console.log(this.parentNode.parentNode.parentNode.querySelector("tr #tableLocation"));;
-  var name = this.parentNode.parentNode.parentNode.querySelector("tr #tableLocation");
+                for (var i = 0; i < result.data.length; i ++) {
+    
+                    var row = `
+    <tr>
+      <td id="tableLocation">${result.data[i].name}<a href="#" id="removeUser" onclick="resetVal()"><i class="fas fa-user-alt-slash" style="float:right; margin-left:50px; color:red;" id="iconSettings"></i></a><a href="#" id="editUser" onclick="valReset()" data-toggle="modal" data-target="#updateModal"><i class="fas fa-user-edit" style="float:right;" id="iconSettings"></i></a></td>
+    </tr>`
+    
+                    table.append(row);
+    
+                }
+            }
+        });
+    }
   
-const location = name.innerText;
-reset();
-  $.ajax({
-      type: 'POST',
-      url: 'companydirectory/libs/php/deleteLocation.php',
-      dataType: 'json',
-      data: {
-        staff: location,
-      },
-      success: function (result) {
-       //$('#deleteModal').modal('toggle');
-
-      }
-      })
-}
-}
-} else {
-console.log("Cancel!");
-}
-
-})
-
-
+  
+  
+  $(document).on('click', '#buttonAdd', function(e) {
+        $.ajax({
+            type: 'POST',
+            url: 'companydirectory/libs/php/addLocation.php',
+            data: {
+                name: $("#fn").val(),
+            },
+            success: function (result) {
+                console.log("New Staff Added!" + result.data);
+               $('#myModal').modal('toggle');
+               reset();
+            }
+        })
+        $(this).unbind('submit').submit()
+    })
+  
+  
+  
+  
+  $(document).on('click', '#removeUser', function(e) {
+      
+    var removeIcons = $("i#iconSettings.fas.fa-user-alt-slash");
+    for (var i = 0; i < removeIcons.length; i++) {
+      removeIcons[i].onclick = function () {
+        console.log(
+          this.parentNode.parentNode.parentNode.querySelector("tr #tableLocation")
+        );
+        var name =
+          this.parentNode.parentNode.parentNode.querySelector("tr #tableLocation");
+    
+        const location = name.innerText;
+    
+        var validate = confirm("Are you sure you want to remove this user?");
+        if (validate == true) {
+          $.ajax({
+            type: "POST",
+            url: "companydirectory/libs/php/deleteLocation.php",
+            dataType: "json",
+            data: {
+              staff: location,
+            },
+            success: function (result) {
+              //$('#deleteModal').modal('toggle');
+              reset();
+            },
+          });
+        } else {
+          console.log("Cancel!");
+        }
+    
+      };
+    }
+  
+  })
+  
+  
  $(document).on('click', '#editUser', function(e) {
   const clickedElement = this.parentNode.parentNode.querySelector("tr #tableLocation").innerText;
    localStorage.clear();
@@ -107,7 +111,7 @@ console.log("Cancel!");
   
   $(document).on('click', '#buttonUpdate', function(e) {
     $('#updateModal').modal('toggle');
-                reset();
+              reset();
     $.ajax({
         type: 'POST',
         url: 'companydirectory/libs/php/updateLocation.php',
@@ -124,6 +128,7 @@ console.log("Cancel!");
     })
   })
 });
-
-
-})
+  
+  
+  })
+  

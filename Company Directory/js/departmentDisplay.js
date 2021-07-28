fec84fcd1923239a652ba5971b7810ac3707fe80
bluezoopla <59399623+Undraoid@users.noreply.github.com>
2021-07-28 21:24:48 +0100

    Update departmentDisplay.js

--- Company Directory/js/departmentDisplay.js
+++ Company Directory/js/departmentDisplay.js
@@ -19,8 +19,8 @@ $(document).ready(function () {
         
                         var row = `
         <tr>
-          <th scope="row" id="tableName">${result.data[i].name}</th>
-          <td id="tableLocation">${locationResult.data[i].name}<a href="#" id="removeUser" onclick="resetVal()"><i class="fas fa-user-alt-slash" style="float:right; margin-left:50px; color:red;" id="iconSettings"></i></a><a href="#" id="editUser" onclick="valReset()" data-toggle="modal" data-target="#updateModal"><i class="fas fa-user-edit" style="float:right;" id="iconSettings"></i></a></td>
+          <th scope="row" id="tableName" data-value=${result.data[i].id}>${result.data[i].name}</th>
+          <td id="tableLocation">${locationResult.data[i].name}<a href="#" id="removeUser" onclick="resetVal() data-toggle="modal" data-target="#ModalConfirm"><i class="fas fa-minus" style="float:right; margin-left:50px; color:red;" id="iconSettings"></i></a><a href="#" id="editUser" onclick="valReset()" data-toggle="modal" data-target="#updateModal"><i class="fas fa-pencil-alt" style="float:right;" id="iconSettings"></i></a></td>
         </tr>`
 
         
@@ -34,6 +34,7 @@ $(document).ready(function () {
         }
     });
 
+  $(".loader").hide();
 
     $.ajax({
         type: 'GET',
@@ -57,37 +58,41 @@ for(i=0;i<result.data.length; i++){
 
 
     function reset(){
+      $(".loader").show();
         $.ajax({
-            type: 'GET',
-            url: 'companydirectory/libs/php/locationID.php',
-            dataType: 'json',
-            success: function (locationResult) {
-    
-                $.ajax({
-                    type: 'GET',
-                    url: 'companydirectory/libs/php/getAllDepartments.php',
-                    dataType: 'json',
-                    success: function (result) {
-                        var table = $("tbody");
-                        table.empty();
-            
-                        for (var i = 0; i < result.data.length; i ++) {
-            
-                            var row = `
-            <tr>
-              <th scope="row" id="tableName">${result.data[i].name}</th>
-              <td id="tableLocation">${locationResult.data[i].name}<a href="#" id="removeUser" onclick="resetVal()"><i class="fas fa-user-alt-slash" style="float:right; margin-left:50px; color:red;" id="iconSettings"></i></a><a href="#" id="editUser" onclick="valReset()" data-toggle="modal" data-target="#updateModal"><i class="fas fa-user-edit" style="float:right;" id="iconSettings"></i></a></td>
-            </tr>`
-            
-                            table.append(row);
-            
-                        }
+        type: 'GET',
+        url: 'companydirectory/libs/php/locationID.php',
+        dataType: 'json',
+        success: function (locationResult) {
+
+            $.ajax({
+                type: 'GET',
+                url: 'companydirectory/libs/php/getAllDepartments.php',
+                dataType: 'json',
+                success: function (result) {
+                    var table = $("tbody");
+                    table.empty();
+        
+                    for (var i = 0; i < result.data.length; i ++) {
+        
+                        var row = `
+        <tr>
+          <th scope="row" id="tableName" data-value=${result.data[i].id}>${result.data[i].name}</th>
+          <td id="tableLocation">${locationResult.data[i].name}<a href="#" id="removeUser" onclick="resetVal() data-toggle="modal" data-target="#ModalConfirm"><i class="fas fa-minus" style="float:right; margin-left:50px; color:red;" id="iconSettings"></i></a><a href="#" id="editUser" onclick="valReset()" data-toggle="modal" data-target="#updateModal"><i class="fas fa-pencil-alt" style="float:right;" id="iconSettings"></i></a></td>
+        </tr>`
+
+        
+                        table.append(row);
+        
                     }
-                });
-    
-    
-            }
-        });
+                }
+            });
+
+
+        }
+    });
+
+  $(".loader").hide();
     }
 
 
@@ -112,41 +117,80 @@ for(i=0;i<result.data.length; i++){
 
 
   
-    $(document).on('click', '#removeUser', function(e) {
+          $(document).on('click', '#removeUser', function(e) {
     
-        var removeIcons = $("i#iconSettings.fas.fa-user-alt-slash");
-        for (var i = 0; i < removeIcons.length; i++) {
-          removeIcons[i].onclick = function () {
-            console.log(
-              this.parentNode.parentNode.parentNode.querySelector("tr #tableName")
-            );
-            var name =
-              this.parentNode.parentNode.parentNode.querySelector("tr #tableName");
-        
-            const location = name.innerText;
-        
-            var validate = confirm("Are you sure you want to remove this user?");
-            if (validate == true) {
-              $.ajax({
-                type: "POST",
-                url: "companydirectory/libs/php/deleteDepartment.php",
-                dataType: "json",
-                data: {
-                  staff: location,
-                },
-                success: function (result) {
-                  //$('#deleteModal').modal('toggle');
-                  reset();
-                },
-              });
-            } else {
-              console.log("Cancel!");
-            }
-        
-          };
-        }
-      
+      var removeIcons = $("i#iconSettings.fas.fa-minus");
+      for (var i = 0; i < removeIcons.length; i++) {
+        removeIcons[i].onclick = function () {
+          console.log(
+            this.parentNode.parentNode.parentNode.querySelector("tr #tableName"),
+          );
+          var name = this.parentNode.parentNode.parentNode.querySelector("tr #tableName");
+          localStorage.setItem("delete",name.innerText)
+          var departmentValue = name.dataset.value;
+          const location = localStorage.getItem("delete")
+
+          $.ajax({
+            type: "GET",
+            url: "companydirectory/libs/php/countDepartment.php",
+            dataType: "json",
+            data: {
+              departmentValue,
+            },
+            success: function (result) {
+              var employees = result.data[0]['count(*)'];
+  console.log(result.data[0]['count(*)']);
+
+    if(employees == 0){
+      $('#ModalConfirm').modal('toggle');
+      $(document).on('click', '#deleteConfirm', function(e) {
+        $.ajax({
+          type: "POST",
+          url: "companydirectory/libs/php/deleteDepartment.php",
+          dataType: "json",
+          data: {
+            staff: location,
+          },
+          success: function (result) {
+            $('#ModalConfirm').modal('toggle');
+            reset();
+          },
+        });
       })
+
+    } else {
+      $('#ModalCancel').modal('toggle');
+      var cancelMessage = $("#cancelMessage")
+      cancelMessage.html(`You cannot delete this department, there are ${employees} employees assigned to it`)
+
+    }
+            },
+          });
+
+      
+         /* var validate = confirm("Are you sure you want to remove this user?");
+          if (validate == true) {
+            $.ajax({
+              type: "POST",
+              url: "companydirectory/libs/php/deleteDepartment.php",
+              dataType: "json",
+              data: {
+                staff: location,
+              },
+              success: function (result) {
+                //$('#deleteModal').modal('toggle');
+                reset();
+              },
+            });
+          } else {
+            console.log("Cancel!");
+          } */
+      
+        };
+      }
+    
+    })
+
 
 
   $(document).on('click', '#editUser', function(e) {
@@ -175,6 +219,13 @@ for(i=0;i<result.data.length; i++){
     })
   });
 
+  
+  $("#searchBar").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("tbody tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
 
 
 

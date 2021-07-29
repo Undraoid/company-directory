c5fb3b92cccd5da31e80e4c168ccd75a22f9a010
bluezoopla <59399623+Undraoid@users.noreply.github.com>
2021-07-29 23:30:47 +0100

    Update dataDisplay.js

--- Company Directory/js/dataDisplay.js
+++ Company Directory/js/dataDisplay.js
@@ -31,6 +31,21 @@ $(document).ready(function () {
         }
     })
   
+  $.ajax({
+        type: 'GET',
+        url: 'companydirectory/libs/php/getAllDepartments.php',
+        dataType: 'json',
+        success: function (result){
+          for(i=0;i< result.data.length; i++){ 
+                    console.log(result.data[i].id);
+               var departments = `
+<option value=${result.data[i].id}>${result.data[i].name}</option>
+`
+               $("#SelectInput3").append(departments);
+          }
+        }
+    })
+  
      $(".loader").hide();
 
     $('#departmentSelect').change(function () {
@@ -65,6 +80,41 @@ $(document).ready(function () {
         });
 
     })
+  
+    function reset(){
+    $(".loader").show();
+       $.ajax({
+            type: 'GET',
+            url: 'companydirectory/libs/php/filterLocationId.php',
+            dataType: 'json',
+            data: {
+                departmentId: $('#departmentSelect').val()
+            },
+            success: function (result) {
+                var table = $("tbody");
+                table.empty();
+
+                for (var i = 0; i < result.data.length; i += 5) {
+                    console.log(result.data[i]);
+
+
+                    var row = `
+    <tr>
+      <th scope="row" id="tableFirstName">${result.data[i].firstName}</th>
+      <td id="tableLastName">${result.data[i].lastName}</td>
+      <td id="tableJobTitle">${result.data[i].jobTitle}</td>
+      <td id="tableEmail">${result.data[i].email}</td>
+      <td id="tableDepartment">${result.data[i].name}<a href="#" id="removeUser" onclick="resetVal()"><i class="fas fa-minus" style="float:right; margin-left:50px; color:red;" id="iconSettings"></i></a><a href="#" id="editUser" onclick="valReset()" data-toggle="modal" data-target="#updateModal"><i class="fas fa-pencil-alt" style="float:right;" id="iconSettings"></i></a></td>
+    </tr>`
+
+                    table.append(row);
+
+                }
+            }
+        });
+
+$(".loader").hide();
+  }
   
 
     $('#InputSelect3').change(function () {
@@ -147,40 +197,59 @@ $(document).ready(function () {
     })
   
 
-    $(document).on('click', '#removeUser', function(e) {
+ $(document).on('click', '#removeUser', function(e) {
     
-        var removeIcons = $("i#iconSettings.fas.fa-minus");
-        for (var i = 0; i < removeIcons.length; i++) {
-          removeIcons[i].onclick = function () {
-            console.log(
-              this.parentNode.parentNode.parentNode.querySelector("tr #tableFirstName")
-            );
-            var name =
-              this.parentNode.parentNode.parentNode.querySelector("tr #tableFirstName");
-        
-            const staff = name.innerText;
-        
-            var validate = confirm("Are you sure you want to remove this user?");
-            if (validate == true) {
-              $.ajax({
-                type: "POST",
-                url: "companydirectory/libs/php/deleteStaff.php",
-                dataType: "json",
-                data: {
-                  staff: staff,
-                },
-                success: function (result) {
-                  //$('#deleteModal').modal('toggle');
-                },
-              });
-            } else {
-              console.log("Cancel!");
-            }
-        
-          };
-        }
-      
+      var removeIcons = $("i#iconSettings.fas.fa-minus");
+      for (var i = 0; i < removeIcons.length; i++) {
+        removeIcons[i].onclick = function () {
+          console.log(this.closest("tr").querySelector("#tableFirstName"));
+          var name = this.closest("tr").querySelector("#tableFirstName");
+          localStorage.setItem("delete",name.innerText)
+          var locationValue = name.dataset.value;
+          console.log(locationValue);
+          const location = localStorage.getItem("delete")
+          $('#ModalConfirm').modal('toggle');
+      $(document).on('click', '#deleteConfirm', function(e) {
+        $.ajax({
+          type: "POST",
+          url: "companydirectory/libs/php/deleteStaff.php",
+          dataType: "json",
+          data: {
+            staff: location,
+          },
+          success: function (result) {
+            $('#ModalConfirm').modal('toggle');
+            reset();
+          },
+        });
       })
+
+  
+      
+         /* var validate = confirm("Are you sure you want to remove this user?");
+          if (validate == true) {
+            $.ajax({
+              type: "POST",
+              url: "companydirectory/libs/php/deleteDepartment.php",
+              dataType: "json",
+              data: {
+                staff: location,
+              },
+              success: function (result) {
+                //$('#deleteModal').modal('toggle');
+                reset();
+              },
+            });
+          } else {
+            console.log("Cancel!");
+          } */
+      
+        };
+      }
+    
+    })
+  
+  
    $(document).on('click', '#editUser', function(e) {
     const clickedElement = this.parentNode.parentNode.querySelector("tr #tableFirstName").innerText;
      localStorage.clear();
@@ -200,8 +269,10 @@ $(document).ready(function () {
           url: 'companydirectory/libs/php/updateStaff.php',
           dataType: 'json',
           data: {
-              info: $('#InputInfo4').val(),
-              staff: $('#changeInput4').val(),
+              firstName: $('#changeInput4').val(),
+              lastName: $('#changeInput5').val(),
+              email: $('#changeInput6').val(),
+              DepartmentNumber: $('#SelectInput3').val(),
               previous: localStorage.getItem("name")
           },
           success: function (result) {
